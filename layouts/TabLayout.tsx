4c12cc779d1a5f6d8fb18606a997d3962a1880b9
Douglas Lowder <douglowder@mac.com>
2025-08-19 19:38:08 -0700

    Native tab layout using router native tabs

--- layouts/TabLayout.tsx
+++ layouts/TabLayout.tsx
@@ -1,69 +1,26 @@
-import React from 'react';
-import { withLayoutContext } from 'expo-router';
-import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation';
-import TabLayoutJS from './TabLayout.web';
-
+import { NativeTabs, Label, Icon } from 'expo-router/unstable-native-tabs';
 import { Platform } from 'react-native';
-import { lightColors, darkColors } from '@/constants/Theme';
-import { useColorScheme } from 'nativewind';
 
-export const Tabs = withLayoutContext(
-  createNativeBottomTabNavigator().Navigator,
-);
+import WebTabLayout from './TabLayout.web';
 
-/**
- * The tab bar for the app.
- * For Android TV, we return the web version.
- * For other native platforms, we use the native tab bar provided by react-native-bottom-tabs.
- */
 export default function TabLayout() {
-  const { colorScheme } = useColorScheme();
-  const themedColors = colorScheme === 'light' ? lightColors : darkColors;
-
   if (Platform.OS === 'android' && Platform.isTV) {
-    return <TabLayoutJS />;
+    return <WebTabLayout />;
   }
   return (
-    <Tabs
-      tabBarActiveTintColor={themedColors['--color-tint']}
-      tabBarInactiveTintColor={themedColors['--color-text']}
-      tabBarStyle={{
-        backgroundColor: themedColors['--color-background'],
-      }}
-      translucent
-      rippleColor={themedColors['--color-background']}
-      labeled={true}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/home.png')
-              : null,
-        }}
-      />
-      <Tabs.Screen
-        name="tvdemo"
-        options={{
-          title: 'Focus/hover/active styles',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/tv.png')
-              : null,
-        }}
-      />
-      <Tabs.Screen
-        name="video"
-        options={{
-          title: 'Video',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/tv.png')
-              : null,
-        }}
-      />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="tvdemo">
+        <Label>Focus/hover/active styles</Label>
+        <Icon sf="tv.fill" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="video">
+        <Label>Video</Label>
+        <Icon sf="video" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
