2d95a6c5b5cbbc6e778d990528280ad947723940
Douglas Lowder <douglowder@mac.com>
2026-02-11 03:11:48 -0800

    Move to new SDK 55 native tabs

--- layouts/TabLayout.tsx
+++ layouts/TabLayout.tsx
@@ -1,5 +1,4 @@
-import { NativeTabs } from 'expo-router/build/native-tabs';
-import { Label, Icon } from 'expo-router';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import { Platform } from 'react-native';
 
 import WebTabLayout from './TabLayout.web';
@@ -11,20 +10,22 @@ export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-        <Icon sf="house" />
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="tvdemo">
-        <Label>Focus/hover/active styles</Label>
-        <Icon sf="tv.fill" />
+        <NativeTabs.Trigger.Label>
+          Focus/hover/active styles
+        </NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="tv.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="video">
-        <Label>Video</Label>
-        <Icon sf="video" />
+        <NativeTabs.Trigger.Label>Video</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="video" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="legendlistdemo">
-        <Label>LegendList</Label>
-        <Icon sf="list.bullet" />
+        <NativeTabs.Trigger.Label>LegendList</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="list.bullet" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
