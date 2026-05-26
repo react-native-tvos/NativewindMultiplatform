0ceea270f2974be288032a15ed36256d67936aa8
Douglas Lowder <douglowder@mac.com>
2026-05-26 11:41:35 -0700

    Add Metro serve-sim support

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,24 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
-console.log(process.ENV);
 const { withNativeWind } = require("nativewind/metro");
+const connect = require("connect");
+const { simMiddleware } = require("serve-sim/middleware");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-module.exports = withNativeWind(config, {
+const config = withNativeWind(getDefaultConfig(__dirname), {
   input: "./global.css",
 });
+
+config.server = config.server || {};
+const originalEnhanceMiddleware = config.server.enhanceMiddleware;
+config.server.enhanceMiddleware = (metroMiddleware, server) => {
+  const middleware = originalEnhanceMiddleware
+    ? originalEnhanceMiddleware(metroMiddleware, server)
+    : metroMiddleware;
+  const app = connect();
+  app.use(simMiddleware({ basePath: "/.sim" }));
+  app.use(middleware);
+  return app;
+};
+
+module.exports = config;
